cb0ef38c4e682f8b8f9d65f0bcfc882b5f0d6426
Marikavka <mkupchinskaia@gmail.com>
2023-04-30 16:46:06 +0800

    Finished project

--- ColorfulView/ViewController.swift
+++ ColorfulView/ViewController.swift
@@ -7,13 +7,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet var colorfulView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorfulView.layer.cornerRadius = 20
+        
+        setupRedSlider()
+        setupGreenSlider()
+        setuoBlueSlider()
+        
+        redValueLabel.text = redSlider.value.formatted()
+        greenValueLabel.text = greenSlider.value.formatted()
+        blueValueLabel.text = blueSlider.value.formatted()
     }
-
-
+    
+    // MARK: - IBActions
+    @IBAction func blueSliderAction() {
+        blueValueLabel.text = (String(format: "%.2f", blueSlider.value))
+        colorfulView.backgroundColor = .init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenValueLabel.text = (String(format: "%.2f", greenSlider.value))
+        colorfulView.backgroundColor = .init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    @IBAction func redSliderAction() {
+        redValueLabel.text = (String(format: "%.2f", redSlider.value))
+        colorfulView.backgroundColor = .init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    // MARK: - Private Methods
+    private func setupRedSlider() {
+        redSlider.tintColor = .red
+        redSlider.thumbTintColor = .lightGray
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.value = 0
+    }
+    
+    private func setupGreenSlider() {
+        greenSlider.tintColor = .green
+        greenSlider.thumbTintColor = .lightGray
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenSlider.value = 0
+    }
+    
+    fileprivate func setuoBlueSlider() {
+        blueSlider.tintColor = .blue
+        blueSlider.thumbTintColor = .lightGray
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueSlider.value = 0
+    }
+    
 }
 
